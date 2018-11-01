6ea9a920253a4210e33998214c36db30ed24f6c7
Langery <1245066197@qq.com>
2018-11-01 23:53:58 +0800

    add files

---
+++ src/components/NewContact.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="newcontact">
+    <el-row>
+      <el-button type="success">1</el-button>
+    </el-row>
+    <el-date-picker
+      v-model="value1"
+      type="date"
+      placeholder="选择日期">
+    </el-date-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value1: ''
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
